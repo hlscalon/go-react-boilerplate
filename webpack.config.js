eca7fd77c36ed87f646088d9efa615ea97740d3f
hlscalon <henscalon@hotmail.com>
2020-02-26 07:46:48 -0300

    added webpack, babel, bulma

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: "./public/index.js",
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/env"]
+                }
+            },
+            {
+                test: /\.(css|scss|sass)$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    output: {
+        path: path.resolve(__dirname, "public/dist/"),
+        publicPath: "/public/dist/",
+        filename: "bundle.js"
+    }
+};
