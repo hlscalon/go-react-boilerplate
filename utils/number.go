60a581a62b6036d4b8f26feb24d41ee8aaf58265
hlscalon <henscalon@hotmail.com>
2020-03-02 07:34:08 -0300

    utils: parse string to integer

---
+++ utils/number.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"strconv"
+)
+
+func StrToInt(str string) (int, error) {
+    if str == "" {
+        return 0, nil
+    }
+
+    i, err := strconv.Atoi(str)
+    if err != nil {
+    	return 0, err
+    }
+
+    return i, nil
+}
