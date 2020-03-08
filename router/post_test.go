a2d19ea877a07b34f3c87e7a9d4021ad5ef32e87
hlscalon <henscalon@hotmail.com>
2020-03-08 09:13:47 -0300

    testing: update post

--- router/post_test.go
+++ router/post_test.go
@@ -5,6 +5,7 @@ import (
     "net/http/httptest"
     "testing"
     "context"
+    "strings"
 
     "github.com/go-chi/chi"
 
@@ -77,6 +78,51 @@ func TestGetPostNotFound(t *testing.T) {
             "\"status\":\"Resource not found.\"" +
         "}" +
         "\n"
+
+    if expected != rec.Body.String() {
+        t.Errorf("\nExpected = %#v\nObtained = %#v", expected, rec.Body.String())
+    }
+}
+
+func TestUpdatePost(t *testing.T) {
+    byteData := `{"id":1,"author":"hlscalon","title":"where I come from","description":"Hello, World!"}`
+    body := strings.NewReader(byteData)
+    req, _ := http.NewRequest("PUT", "/", body) // api/admin/v1/posts/1
+    req.Header.Set("Content-Type", "application/json")
+
+    rctx := chi.NewRouteContext()
+    rctx.URLParams.Add("postID", "1")
+
+    req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+
+    rec := httptest.NewRecorder()
+    env := Env{db: &models.MockDB{}}
+    env.postCtx(http.HandlerFunc(env.updatePost)).ServeHTTP(rec, req)
+
+    expected :=
+        byteData +
+        "\n"
+
+    if expected != rec.Body.String() {
+        t.Errorf("\nExpected = %#v\nObtained = %#v", expected, rec.Body.String())
+    }
+}
+
+func TestUpdatePostInvalidContentType(t *testing.T) {
+    byteData := `{"id":1,"author":"hlscalon","title":"where I come from","description":"Hello, World!"}`
+    body := strings.NewReader(byteData)
+    req, _ := http.NewRequest("PUT", "/", body) // api/admin/v1/posts/1
+
+    rctx := chi.NewRouteContext()
+    rctx.URLParams.Add("postID", "1")
+
+    req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+
+    rec := httptest.NewRecorder()
+    env := Env{db: &models.MockDB{}}
+    env.postCtx(http.HandlerFunc(env.updatePost)).ServeHTTP(rec, req)
+
+    expected := "{\"status\":\"Invalid request.\",\"error\":\"render: unable to automatically decode the request content type\"}\n"
 
     if expected != rec.Body.String() {
         t.Errorf("\nExpected = %#v\nObtained = %#v", expected, rec.Body.String())
