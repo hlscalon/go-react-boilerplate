c3bc67c78ede2f7dccebaf26140818e9a2be4f93
hlscalon <henscalon@hotmail.com>
2020-03-10 07:29:44 -0300

    fix formatting

--- models/post_mock.go
+++ models/post_mock.go
@@ -7,13 +7,13 @@ import (
 type MockDB struct{}
 
 var postsMockDB = []*Post{
-    &Post{1, "hlscalon", "hello", "Hello, World!"},
-    &Post{2, "user", "summer", "Summer is ending!"},
-    &Post{3, "tester", "day", "Today is a sunny day"},
+	&Post{1, "hlscalon", "hello", "Hello, World!"},
+	&Post{2, "user", "summer", "Summer is ending!"},
+	&Post{3, "tester", "day", "Today is a sunny day"},
 }
 
 func (mdb *MockDB) AllPosts() ([]*Post, error) {
-    return postsMockDB, nil
+	return postsMockDB, nil
 }
 
 func (mdb *MockDB) Post(ID int) (*Post, error) {
@@ -23,47 +23,46 @@ func (mdb *MockDB) Post(ID int) (*Post, error) {
 		}
 	}
 
-    return nil, errors.New("Resource not found")
+	return nil, errors.New("Resource not found")
 }
 
 func (mdb *MockDB) UpdatePost(post *Post) (*Post, error) {
-    for _, p := range postsMockDB {
-        if p.ID == post.ID {
-            p = post
+	for _, p := range postsMockDB {
+		if p.ID == post.ID {
+			p = post
 
-            return post, nil
-        }
-    }
+			return post, nil
+		}
+	}
 
-    return nil, errors.New("Resource not found")
+	return nil, errors.New("Resource not found")
 }
 
 func (mdb *MockDB) CreatePost(post *Post) (*Post, error) {
-    lastID := postsMockDB[len(postsMockDB) - 1 : len(postsMockDB)][0].ID
+	lastID := postsMockDB[len(postsMockDB)-1 : len(postsMockDB)][0].ID
 
-    post.ID = lastID + 1
-    postsMockDB = append(postsMockDB, post)
+	post.ID = lastID + 1
+	postsMockDB = append(postsMockDB, post)
 
-    return post, nil
+	return post, nil
 }
 
 func (mdb *MockDB) DeletePost(ID int) (*Post, error) {
-    var posts []*Post
-    var post *Post
+	var posts []*Post
+	var post *Post
 
-    for _, p := range postsMockDB {
-        if p.ID == ID {
-            post = p
-        } else {
-            posts = append(posts, p)
-        }
-    }
+	for _, p := range postsMockDB {
+		if p.ID == ID {
+			post = p
+		} else {
+			posts = append(posts, p)
+		}
+	}
 
-    postsMockDB = posts
-    if post != nil {
-        return post, nil
-    }
+	postsMockDB = posts
+	if post != nil {
+		return post, nil
+	}
 
-    return nil, errors.New("Resource not found")
+	return nil, errors.New("Resource not found")
 }
-
--- router/post_test.go
+++ router/post_test.go
@@ -1,166 +1,166 @@
 package router
 
 import (
-    "net/http"
-    "net/http/httptest"
-    "testing"
-    "context"
-    "strings"
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 
-    "github.com/go-chi/chi"
+	"github.com/go-chi/chi"
 
-    "github.com/hlscalon/go-react-boilerplate/models"
+	"github.com/hlscalon/go-react-boilerplate/models"
 )
 
 func TestListPosts(t *testing.T) {
-    rec := httptest.NewRecorder()
-    req, _ := http.NewRequest("GET", "/", nil) // api/public/v1/posts
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil) // api/public/v1/posts
 
-    env := Env{db: &models.MockDB{}}
-    http.HandlerFunc(env.listPosts).ServeHTTP(rec, req)
+	env := Env{db: &models.MockDB{}}
+	http.HandlerFunc(env.listPosts).ServeHTTP(rec, req)
 
-    expected :=
-        "[" +
-            "{" +
-                "\"id\":1,\"author\":\"hlscalon\",\"title\":\"hello\",\"description\":\"Hello, World!\"" +
-            "}," +
-            "{" +
-                "\"id\":2,\"author\":\"user\",\"title\":\"summer\",\"description\":\"Summer is ending!\"" +
-            "}," +
-            "{" +
-                "\"id\":3,\"author\":\"tester\",\"title\":\"day\",\"description\":\"Today is a sunny day\"" +
-            "}" +
-        "]" +
-        "\n"
+	expected :=
+		"[" +
+			"{" +
+			"\"id\":1,\"author\":\"hlscalon\",\"title\":\"hello\",\"description\":\"Hello, World!\"" +
+			"}," +
+			"{" +
+			"\"id\":2,\"author\":\"user\",\"title\":\"summer\",\"description\":\"Summer is ending!\"" +
+			"}," +
+			"{" +
+			"\"id\":3,\"author\":\"tester\",\"title\":\"day\",\"description\":\"Today is a sunny day\"" +
+			"}" +
+			"]" +
+			"\n"
 
-    if rec.Code != 200 || expected != rec.Body.String() {
-        t.Errorf("\nExpected = %#v\nObtained = %#v\nExpected = %#v\nObtained = %#v", 200, rec.Code, expected, rec.Body.String())
-    }
+	if rec.Code != 200 || expected != rec.Body.String() {
+		t.Errorf("\nExpected = %#v\nObtained = %#v\nExpected = %#v\nObtained = %#v", 200, rec.Code, expected, rec.Body.String())
+	}
 }
 
 func TestGetPost(t *testing.T) {
-    req, _ := http.NewRequest("GET", "/", nil) // api/public/v1/posts/1
+	req, _ := http.NewRequest("GET", "/", nil) // api/public/v1/posts/1
 
-    rctx := chi.NewRouteContext()
-    rctx.URLParams.Add("postID", "1")
+	rctx := chi.NewRouteContext()
+	rctx.URLParams.Add("postID", "1")
 
-    req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
 
-    rec := httptest.NewRecorder()
-    env := Env{db: &models.MockDB{}}
-    env.postCtx(http.HandlerFunc(env.getPost)).ServeHTTP(rec, req)
+	rec := httptest.NewRecorder()
+	env := Env{db: &models.MockDB{}}
+	env.postCtx(http.HandlerFunc(env.getPost)).ServeHTTP(rec, req)
 
-    expected :=
-        "{" +
-            "\"id\":1,\"author\":\"hlscalon\",\"title\":\"hello\",\"description\":\"Hello, World!\"" +
-        "}" +
-        "\n"
+	expected :=
+		"{" +
+			"\"id\":1,\"author\":\"hlscalon\",\"title\":\"hello\",\"description\":\"Hello, World!\"" +
+			"}" +
+			"\n"
 
-    if rec.Code != 200 || expected != rec.Body.String() {
-        t.Errorf("\nExpected = %#v\nObtained = %#v\nExpected = %#v\nObtained = %#v", 200, rec.Code, expected, rec.Body.String())
-    }
+	if rec.Code != 200 || expected != rec.Body.String() {
+		t.Errorf("\nExpected = %#v\nObtained = %#v\nExpected = %#v\nObtained = %#v", 200, rec.Code, expected, rec.Body.String())
+	}
 }
 
 func TestGetPostNotFound(t *testing.T) {
-    req, _ := http.NewRequest("GET", "/", nil) // api/public/v1/posts/1
+	req, _ := http.NewRequest("GET", "/", nil) // api/public/v1/posts/1
 
-    rctx := chi.NewRouteContext()
-    rctx.URLParams.Add("postID", "10000")
+	rctx := chi.NewRouteContext()
+	rctx.URLParams.Add("postID", "10000")
 
-    req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
 
-    rec := httptest.NewRecorder()
-    env := Env{db: &models.MockDB{}}
-    env.postCtx(http.HandlerFunc(env.getPost)).ServeHTTP(rec, req)
+	rec := httptest.NewRecorder()
+	env := Env{db: &models.MockDB{}}
+	env.postCtx(http.HandlerFunc(env.getPost)).ServeHTTP(rec, req)
 
-    expected :=
-        "{" +
-            "\"status\":\"Resource not found.\"" +
-        "}" +
-        "\n"
+	expected :=
+		"{" +
+			"\"status\":\"Resource not found.\"" +
+			"}" +
+			"\n"
 
-    if rec.Code != 404 || expected != rec.Body.String() {
-        t.Errorf("\nExpected = %#v\nObtained = %#v\nExpected = %#v\nObtained = %#v", 404, rec.Code, expected, rec.Body.String())
-    }
+	if rec.Code != 404 || expected != rec.Body.String() {
+		t.Errorf("\nExpected = %#v\nObtained = %#v\nExpected = %#v\nObtained = %#v", 404, rec.Code, expected, rec.Body.String())
+	}
 }
 
 func TestUpdatePost(t *testing.T) {
-    byteData := `{"id":1,"author":"hlscalon","title":"where I come from","description":"Hello, World!"}`
-    body := strings.NewReader(byteData)
-    req, _ := http.NewRequest("PUT", "/", body) // api/admin/v1/posts/1
-    req.Header.Set("Content-Type", "application/json")
+	byteData := `{"id":1,"author":"hlscalon","title":"where I come from","description":"Hello, World!"}`
+	body := strings.NewReader(byteData)
+	req, _ := http.NewRequest("PUT", "/", body) // api/admin/v1/posts/1
+	req.Header.Set("Content-Type", "application/json")
 
-    rctx := chi.NewRouteContext()
-    rctx.URLParams.Add("postID", "1")
+	rctx := chi.NewRouteContext()
+	rctx.URLParams.Add("postID", "1")
 
-    req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
 
-    rec := httptest.NewRecorder()
-    env := Env{db: &models.MockDB{}}
-    env.postCtx(http.HandlerFunc(env.updatePost)).ServeHTTP(rec, req)
+	rec := httptest.NewRecorder()
+	env := Env{db: &models.MockDB{}}
+	env.postCtx(http.HandlerFunc(env.updatePost)).ServeHTTP(rec, req)
 
-    expected :=
-        byteData +
-        "\n"
+	expected :=
+		byteData +
+			"\n"
 
-    if rec.Code != 200 || expected != rec.Body.String() {
-        t.Errorf("\nExpected = %#v\nObtained = %#v\nExpected = %#v\nObtained = %#v", 200, rec.Code, expected, rec.Body.String())
-    }
+	if rec.Code != 200 || expected != rec.Body.String() {
+		t.Errorf("\nExpected = %#v\nObtained = %#v\nExpected = %#v\nObtained = %#v", 200, rec.Code, expected, rec.Body.String())
+	}
 }
 
 func TestUpdatePostInvalidContentType(t *testing.T) {
-    byteData := `{"id":1,"author":"hlscalon","title":"where I come from","description":"Hello, World!"}`
-    body := strings.NewReader(byteData)
-    req, _ := http.NewRequest("PUT", "/", body) // api/admin/v1/posts/1
+	byteData := `{"id":1,"author":"hlscalon","title":"where I come from","description":"Hello, World!"}`
+	body := strings.NewReader(byteData)
+	req, _ := http.NewRequest("PUT", "/", body) // api/admin/v1/posts/1
 
-    rctx := chi.NewRouteContext()
-    rctx.URLParams.Add("postID", "1")
+	rctx := chi.NewRouteContext()
+	rctx.URLParams.Add("postID", "1")
 
-    req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
 
-    rec := httptest.NewRecorder()
-    env := Env{db: &models.MockDB{}}
-    env.postCtx(http.HandlerFunc(env.updatePost)).ServeHTTP(rec, req)
+	rec := httptest.NewRecorder()
+	env := Env{db: &models.MockDB{}}
+	env.postCtx(http.HandlerFunc(env.updatePost)).ServeHTTP(rec, req)
 
-    expected := "{\"status\":\"Invalid request.\",\"error\":\"render: unable to automatically decode the request content type\"}\n"
+	expected := "{\"status\":\"Invalid request.\",\"error\":\"render: unable to automatically decode the request content type\"}\n"
 
-    if rec.Code != 400 || expected != rec.Body.String() {
-        t.Errorf("\nExpected = %#v\nObtained = %#v\nExpected = %#v\nObtained = %#v", 400, rec.Code, expected, rec.Body.String())
-    }
+	if rec.Code != 400 || expected != rec.Body.String() {
+		t.Errorf("\nExpected = %#v\nObtained = %#v\nExpected = %#v\nObtained = %#v", 400, rec.Code, expected, rec.Body.String())
+	}
 }
 
 func TestCreatePost(t *testing.T) {
-    byteData := `{"author":"batman","title":"Gotham I'm coming!","description":"All criminals in town, be aware!"}`
-    body := strings.NewReader(byteData)
-    req, _ := http.NewRequest("POST", "/", body) // api/admin/v1/posts/
-    req.Header.Set("Content-Type", "application/json")
+	byteData := `{"author":"batman","title":"Gotham I'm coming!","description":"All criminals in town, be aware!"}`
+	body := strings.NewReader(byteData)
+	req, _ := http.NewRequest("POST", "/", body) // api/admin/v1/posts/
+	req.Header.Set("Content-Type", "application/json")
 
-    rec := httptest.NewRecorder()
-    env := Env{db: &models.MockDB{}}
-    http.HandlerFunc(env.createPost).ServeHTTP(rec, req)
+	rec := httptest.NewRecorder()
+	env := Env{db: &models.MockDB{}}
+	http.HandlerFunc(env.createPost).ServeHTTP(rec, req)
 
-    expected := "{\"id\":4,\"author\":\"batman\",\"title\":\"Gotham I'm coming!\",\"description\":\"All criminals in town, be aware!\"}\n"
+	expected := "{\"id\":4,\"author\":\"batman\",\"title\":\"Gotham I'm coming!\",\"description\":\"All criminals in town, be aware!\"}\n"
 
-    if rec.Code != 201 || expected != rec.Body.String() {
-        t.Errorf("\nExpected = %#v\nObtained = %#v\nExpected = %#v\nObtained = %#v", 201, rec.Code, expected, rec.Body.String())
-    }
+	if rec.Code != 201 || expected != rec.Body.String() {
+		t.Errorf("\nExpected = %#v\nObtained = %#v\nExpected = %#v\nObtained = %#v", 201, rec.Code, expected, rec.Body.String())
+	}
 }
 
 func TestDeletePost(t *testing.T) {
-    req, _ := http.NewRequest("DELETE", "/", nil) // api/admin/v1/posts/4
+	req, _ := http.NewRequest("DELETE", "/", nil) // api/admin/v1/posts/4
 
-    rctx := chi.NewRouteContext()
-    rctx.URLParams.Add("postID", "4")
+	rctx := chi.NewRouteContext()
+	rctx.URLParams.Add("postID", "4")
 
-    req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
 
-    rec := httptest.NewRecorder()
-    env := Env{db: &models.MockDB{}}
-    env.postCtx(http.HandlerFunc(env.deletePost)).ServeHTTP(rec, req)
+	rec := httptest.NewRecorder()
+	env := Env{db: &models.MockDB{}}
+	env.postCtx(http.HandlerFunc(env.deletePost)).ServeHTTP(rec, req)
 
-    expected := "{\"id\":4,\"author\":\"batman\",\"title\":\"Gotham I'm coming!\",\"description\":\"All criminals in town, be aware!\"}\n"
+	expected := "{\"id\":4,\"author\":\"batman\",\"title\":\"Gotham I'm coming!\",\"description\":\"All criminals in town, be aware!\"}\n"
 
-    if rec.Code != 200 || expected != rec.Body.String() {
-        t.Errorf("\nExpected = %#v\nObtained = %#v\nExpected = %#v\nObtained = %#v", 200, rec.Code, expected, rec.Body.String())
-    }
+	if rec.Code != 200 || expected != rec.Body.String() {
+		t.Errorf("\nExpected = %#v\nObtained = %#v\nExpected = %#v\nObtained = %#v", 200, rec.Code, expected, rec.Body.String())
+	}
 }
